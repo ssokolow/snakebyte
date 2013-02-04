e5012dc437bfb4fdd55d0da75397cf1edbfccde6
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2013-02-04 15:41:51 -0500

    Add branch coverage to the test runs.
    (4 branches out of 331 untested... not bad for a start.)

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #TODO: Integrate this into setup.py once I can make "setup.py install" work
 
-nosetests -v --with-coverage --cover-tests || status=$?
+nosetests -v --with-coverage --cover-tests --cover-branches || status=$?
 pyflakes *.py || status=$(($? || status))
 pep8 --show-source --show-pep8 *.py || status=$(($? || status))
 epydoc -v --docformat=restructuredtext --fail-on-docstring-warning *.py || status=$(($? || status))
