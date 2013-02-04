537d7fec192448efc50c42283d12ba4b680ff6c1
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2013-02-04 16:02:30 -0500

    Whoops. Forgot a couple of test dependencies and an epydoc flag.

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@
 nosetests -v --with-coverage --cover-tests --cover-branches || status=$?
 pyflakes *.py || status=$(($? || status))
 pep8 --show-source --show-pep8 *.py || status=$(($? || status))
-epydoc -v --docformat=restructuredtext --fail-on-docstring-warning *.py || status=$(($? || status))
+epydoc -v --simple-term --docformat=restructuredtext --fail-on-docstring-warning *.py || status=$(($? || status))
 exit $status
