bd1964f782af2c6a0972dcbc4a1be967b290b242
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2013-03-23 21:59:03 -0400

    Make setup.py EPyDoc-friendly and work around a revealed PyPy segfault.
    (Should've known forgetting to run tox AND ./run_test.py would bite me.)

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
-from setuptools import setup
+import sys
 
-setup(
-    name="SnakeByte",
-    version="0.1",
-    description="Python components for building solid IRC scripts.",
-    long_description="""\
+if __name__ == '__main__' and not 'flake8' in sys.modules:
+    #FIXME: Why does this segfault flake8 under PyPy?
+    from setuptools import setup
+
+    setup(
+        name="SnakeByte",
+        version="0.1",
+        description="Python components for building solid IRC scripts.",
+        long_description="""\
 While not yet usable on its own, this package will eventually provide a
 complete set of components for building all common types of IRC scripts
 quickly, easily, and reliably.""",
-    author="Stephan Sokolow",
-    author_email="http://www.ssokolow.com/ContactMe",  # No spam harvesting
-    url="http://ssokolow.com/snakebyte/",
-    packages=['snakebyte'],
+        author="Stephan Sokolow",
+        author_email="http://www.ssokolow.com/ContactMe",  # No spam harvesting
+        url="http://ssokolow.com/snakebyte/",
+        packages=['snakebyte'],
 
-    download_url="https://github.com/ssokolow/snakebyte/",
-    zip_safe=True
-)
+        download_url="https://github.com/ssokolow/snakebyte/",
+        zip_safe=True
+    )
